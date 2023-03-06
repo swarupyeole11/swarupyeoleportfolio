c19de1cb9e7f02a62940e82cae8eb74432769de9
swarupyeole11 <swarupyeole11@gmail.com>
2023-03-06 12:24:51 +0530

    projectsetup

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      
+     <div className='bg-black'>
+        <p>hello</p>
+        <h1 className='text-2xl'>hola</h1>
+     </div>
+    </div>
+  )
+}
+
+export default App
